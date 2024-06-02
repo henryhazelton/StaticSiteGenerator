423ece3b8d0f3a3fc91d28f293b37a7101075d8f
henryhazelton <henryhazelton00@gmail.com>
2024-06-02 15:20:30 +0100

    First attempt at writing functions to extract title from markdown and then generate a page

--- src/functions_website_generation.py
+++ src/functions_website_generation.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+from functions_block_markdown import markdown_to_html_node
+
 def copy_directory(src, dest):
     clear_directory(dest)
     # I need this function to copy all the data and files in one directory and then paste them into another directory, we can do this!
@@ -30,3 +32,38 @@ def clear_directory(directory):
     print(f"Created clean directory: {directory}")
     return clean_directory
 # This function removes the destination directory and then recreates it as a clean one each time
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# ") not in lines:
+            raise Exception("Markdown file must contain a h1 heading")
+        if line.startswith("# "):
+            return line
+    
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    # Below will read the file from "from_path"
+    markdown_file = open("content/index.md", "r")
+    markdown_file_contents = markdown_file.read()
+    markdown_file.close()
+    # This section above should open the markdown file located at "from_path", store the contents of the file in the variable "markdown_file_contents"
+    # and then close the file which we opened.
+    # We will now repeat for the file located at "template_path"
+    template_file = open("template.html", "r")
+    template_file_contents = template_file.read()
+    template_file.close()
+    markdown_file_to_HTML = markdown_to_html_node(markdown_file_contents)
+    title = str(extract_title(markdown_file_contents))
+    HTML_file = template_file_contents.replace("{{ Title }}", title)
+    HTML_file = HTML_file.replace("{{ Content }}", str(markdown_file_contents))
+    copy_directory(from_path, dest_path)
+    for item in os.listdir(from_path):
+        src_path = os.path.join(from_path, item)
+        dest_path = os.path.join(dest_path, item)
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        
+
+
+# Im not sure the above is right, markdown file to html is now HTML not markup, so I need to extract the title from markdown, not html
--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 print("Hello World!")
 
 from textnode import TextNode
-from functions_website_generation import clear_directory, copy_directory
+from functions_website_generation import clear_directory, copy_directory, generate_page, extract_title
+
+src_path_static = "./static"
+dest_path_static = "./public"
 
 def main():
     node = TextNode('Some Text', 'Bold', 'https://example.com')
@@ -12,7 +15,9 @@ def main():
 
     print(node2)
 
-    copy_directory('static', 'public')
+    copy_directory(src_path_static, dest_path_static)
+
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
